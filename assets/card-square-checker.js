cc2e992249154c2ceb4046ee488d787ec54291b4
William Nyunt <william.nyunt@7dots.co.uk>
2022-07-13 17:20:09 +0100

    Product cards colour-change
    All cards have white background
    Sections containing these cards have semi white background

---
+++ assets/card-square-checker.js
@@ -0,0 +1,18 @@
+class cardChecker {
+    constructor(className){
+          this.cardImages = document.querySelectorAll(className)  
+    }
+    /* 
+    * Compare if square and adds '-apply-square' class to class list if true
+    */
+    applyImageSquare(){
+        this.cardImages.forEach((cardImage) => {
+               if (cardImage.naturalHeight == cardImage.naturalWidth){
+                cardImage.classList.add('-apply-square')
+            }
+        })
+    }
+}
+
+let productCard = new cardChecker('.card-product__image');
+productCard.applyImageSquare();
