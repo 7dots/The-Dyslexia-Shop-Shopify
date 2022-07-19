e2795466506ac8162cfcb55d0c17214d11c9e581
Jon Warde <jon.warde@7dots.co.uk>
2022-07-19 15:31:47 +0100

    Add class toggle to button onclick

---
+++ assets/product-grid-layout-switch.js
@@ -0,0 +1,12 @@
+const productGridLayoutSwitch = () => {
+    const productGrid = document.getElementById('product-grid')
+    const productGridLayoutSwitch = document.querySelector('.product-grid-layout-switch');
+    if(!productGrid && productGridLayoutSwitch){
+        return;
+    }
+    productGridLayoutSwitch.addEventListener('click', () => {
+        productGrid.classList.toggle('--layout-list')
+    })
+}
+productGridLayoutSwitch()
+
