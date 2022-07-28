82a36772dc4bfe8a73c4f972f7f3d9f497a7eabc
William Nyunt <william.nyunt@7dots.co.uk>
2022-07-28 15:41:48 +0100

    Clerkio carousels aligns

--- assets/clerkio-recommendations.js
+++ assets/clerkio-recommendations.js
@@ -2,13 +2,20 @@
 
 window.addEventListener('load', (event) => {
     clerkioWrappers = document.querySelectorAll('.js-clerkio-recommendations')
+
     clerkioWrappers.forEach(element => {
-        clerkioSlider = element.querySelector('.clerk-slider')
-        forwardBtn = element.querySelector('.clerk-slider-nav-next')
-        backBtn = element.querySelector('.clerk-slider-nav-prev')
 
+    clerkioSlider = element.querySelector('.clerk-slider')
+    forwardBtn = element.querySelector('.clerk-slider-nav-next')
+    backBtn = element.querySelector('.clerk-slider-nav-prev')
+    clerkioSlider = element.querySelector('.clerk-slider')
+    clerkioSliderItems = clerkioSlider.querySelectorAll('.clerk-slider-item')
+
+    ButtonCountCheck()
+    setHeightMax()
+
+    function ButtonCountCheck(){
         const itemPerSlide = 4;
-
         const itemCount = clerkioSlider.children.length
 
         //+2 is there because forward and backwards btns are in the .clerk-slider  
@@ -16,5 +23,22 @@ window.addEventListener('load', (event) => {
             forwardBtn.remove()
             backBtn.remove()
         }
+    }
+
+    function setHeightMax(){
+        let itemHeight = 0
+        console.log(clerkioSliderItems)
+        clerkioSliderItems.forEach(e => {
+            if (e.clientHeight > itemHeight){
+                itemHeight = e.clientHeight
+                console.log('true')
+            }
+        })
+
+        clerkioSliderItems.forEach(e => {
+            e.style.height = itemHeight+'px';
+        })
+    }
     });
 });
+
