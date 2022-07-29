0561c9eddd37e669bf661a53b1bff255997b8957
Jon Warde <jon.warde@7dots.co.uk>
2022-07-29 11:25:04 +0100

    Modify regex for reviews string

--- assets/ekomi-product-reviews.js
+++ assets/ekomi-product-reviews.js
@@ -2,14 +2,16 @@
  * Ekomi product reviews API
  * Retrieve and display product reviews from Ekomi
  * @author Jon Warde 7dots.co.uk
- * @param string product_sku or 'all'
  * @type {{redirect: string, method: string}}
  */
-const getEkomiProductReviews = ($product_sku) => {
+const getEkomiProductReviews = () => {
+
+    const reviewsHolder = document.getElementById('ekomi-product-reviews');
+    const productSKU = reviewsHolder.getAttribute('data-sku');
     const baseCredentials = {
         ekomiApi:'https://api.ekomi.de/get_productfeedback.php',
-        apiid: 'APIID',
-        apiPass: 'APIPASS'
+        apiid: '52635',
+        apiPass: '7918dcccc263ae30ca8f2aa0d'
     }
     const requestOptions = {
         method: 'GET',
@@ -19,30 +21,37 @@ const getEkomiProductReviews = ($product_sku) => {
 
     const getArrayFromResults = (result) => {
 
+        console.log(result);
+
         // Results come back as a single string - not json (not ideal) ... which needs to be split into a managable array.
         // String has comma seperated [timestamp, product code, sku code, text review]
-        let productCodes = /[0-9]+[,][0-9]+[,][A-Z0-9]+[,]/g;
+
+        // TODO: FIX FOR THIS FORMAT
+        // 1415625910,100022331,MS010-Aqua,4,"Matched requirements"
+        let productCodes = /[0-9]+[,][0-9]+[,][A-Za-z0-9-]+[,]/g;
         const resultAsArr = result.split(productCodes);
         // save up out markup to spit on the page
         var reviewsMarkup = '';
         resultAsArr.forEach((r) => {
             // split rating from review
-            let _rating = r.split(',"');
+            let _rating = r.split(',');
+
+            console.log('FIRST: '+ _rating[0] + ' SECOND: ' + _rating[1]);
+
             if(_rating[0] != '' && _rating[1] != '') {
                 let customerReview = `<p>${_rating[0]}/5 stars <span>${_rating[1]}</span></p>`;
                 reviewsMarkup += customerReview;
             }
+
         })
         // Add reviews to the page
-        const reviewsHolder = document.getElementById('ekomi-product-reviews');
         reviewsHolder.innerHTML = reviewsMarkup;
-
     }
 
-    fetch(baseEndPoint + $product_sku, requestOptions)
+    fetch(baseEndPoint + productSKU, requestOptions)
         .then(response => response.text())
         .then(result => getArrayFromResults(result))
         .catch(error => console.log('error', error));
 
 }
-getEkomiProductReviews('MS003')
+getEkomiProductReviews()
