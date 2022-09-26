d427849d62c9af20cfcbea7ad2c743cebe45ef6e
William Nyunt <william.nyunt@7dots.co.uk>
2022-09-26 17:32:06 +0100

    Search-page/Search-bar style

---
+++ assets/clerk-search.js
@@ -0,0 +1,10 @@
+searchInput = document.querySelector('#Search-In-Modal');
+
+//will need to perform on key press and key chamge
+searchInput.addEventListener("keypress", function(){
+    console.log('key has been pressed')
+})
+
+let highlighted = document.querySelectorAll('.clerk-highlighted')
+console.log(highlighted)
+
